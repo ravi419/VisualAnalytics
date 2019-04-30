1d48a4ff8d244e92c1feb840305d27d295f7b630
Ravi <ravi.mallikarjun07@gmail.com>
2019-04-30 13:51:56 +0200

    reduced the complexity using 'repeat if' loop

--- Exercise3/Exercise_3.R
+++ Exercise3/Exercise_3.R
@@ -1,18 +1,17 @@
 library(dplyr)
 
-
-
-
 x <- c(2.0,2.0,2.0,2.5,2.5,3.0,4.0,4.0,4.5,4.5,4.5,4.5)
 y <- c(1.0,1.5,2.0,1.0,2.0,4.0,1.0,2.5,1.0,1.5,2.5,3.0)
 index <- c("p1","p2","p3","p4","p5","p6","p7","p8","p9","p10","p11","p12")
 
 DF <- data.frame(index, x, y, stringsAsFactors=FALSE)
 
-CurrentCentroids <- (DF[c(1,2,3),c("index","x","y")])
+PreviousCentroids <- (DF[c(1,2,3),c("index","x","y")])
 
-
-
+iter <-1
+repeat{
+  
+  CurrentCentroids <- PreviousCentroids
 Distance <- apply(CurrentCentroids,1,function(CurrentCentroids)
   (apply(DF,1,function(DF,CurrentCentroids)
     dist(rbind(DF,CurrentCentroids)),CurrentCentroids)))
@@ -29,38 +28,42 @@ DF2 <- merge(DF, Distance, by="index")
 DF2 <- DF2 %>%
   select(index,x, y, p1, p2, p3, Clusters) %>% group_by(Clusters)
 
-ChangedCentroids <-  DF2 %>%  summarise(x = mean(x),
+PreviousCentroids <-  DF2 %>%  summarise(x = mean(x),
                                        y = mean(y))
+iter <- iter +1
 
-while (CurrentCentroids != ChangedCentroids) {
-  CurrentCentroids <- NULL
-  CurrentCentroids <- ChangedCentroids
-  ChangedCentroids <- NULL
-  
-  DistanceCalculation(CurrentCentroids,DF)
+if (all(CurrentCentroids == PreviousCentroids)) {
+  break
+ # DistanceCalculation(CurrentCentroids,DF)
+}
 }
 
+# DistanceCalculation <- function(CurrentCentroids,DF) 
+# {
+#   Distance <- apply(CurrentCentroids,1,function(CurrentCentroids)
+#     (apply(DF,1,function(DF,CurrentCentroids)
+#       dist(rbind(DF,CurrentCentroids)),CurrentCentroids)))
+#   
+#   Distance <- data.frame(Distance)
+#   
+#   cols <- c("p1","p2","p3")
+#   colnames(Distance) <- cols
+#   
+#   Distance$Clusters <- colnames(Distance)[apply(Distance,1,which.min)]
+#   Distance$index <-index
+#   DF2 <- merge(DF, Distance, by="index")
+#   
+#   DF2 <- DF2 %>%
+#     select(index,x, y, p1, p2, p3, Clusters) %>% group_by(Clusters)
+#   
+#   PreviousCentroids <-  DF2 %>%  summarise(x = mean(x),
+#                                           y = mean(y))
+#   
+#   
+# }
+# 
+# repeat{ # in R
+#   ..statements..
+#   if(! condition){ break } # Negation is crucial here!
+# }
 
-DistanceCalculation <- function(CurrentCentroids,DF) 
-{
-  Distance <- apply(CurrentCentroids,1,function(CurrentCentroids)
-    (apply(DF,1,function(DF,CurrentCentroids)
-      dist(rbind(DF,CurrentCentroids)),CurrentCentroids)))
-  
-  Distance <- data.frame(Distance)
-  
-  cols <- c("p1","p2","p3")
-  colnames(Distance) <- cols
-  
-  Distance$Clusters <- colnames(Distance)[apply(Distance,1,which.min)]
-  Distance$index <-index
-  DF2 <- merge(DF, Distance, by="index")
-  
-  DF2 <- DF2 %>%
-    select(index,x, y, p1, p2, p3, Clusters) %>% group_by(Clusters)
-  
-  ChangedCentroids <-  DF2 %>%  summarise(x = mean(x),
-                                          y = mean(y))
-  
-  
-}
